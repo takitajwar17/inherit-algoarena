3de94bf581e23f9e7c7ac26d534a7f0a116eccc3
takitajwar17 <tajwaruzzaman@iut-dhaka.edu>
2025-05-07 11:09:30 +0600

    Minor changes

--- app/components/LandingPage.jsx
+++ app/components/LandingPage.jsx
@@ -128,7 +128,7 @@ const LandingPage = () => {
                 SDGs
               </Link>
               <Link href={isSignedIn ? "/dashboard" : "/sign-in"}>
-                <button className="bg-gray-600 text-white px-6 py-2 rounded-full text-lg font-semibold hover:bg-indigo-700 transition-colors">
+                <button className="bg-[#000033] text-white px-6 py-2 rounded-full text-lg font-semibold hover:bg-indigo-700 transition-colors">
                   Get Started
                 </button>
               </Link>
@@ -164,7 +164,7 @@ const LandingPage = () => {
                   href={isSignedIn ? "/dashboard" : "/sign-in"}
                   onClick={() => setIsMenuOpen(false)}
                 >
-                  <button className="bg-gray-600 text-white px-6 py-2 rounded-full text-lg font-semibold hover:bg-indigo-700 transition-colors w-full">
+                  <button className="bg-[#000033] text-white px-6 py-2 rounded-full text-lg font-semibold hover:bg-indigo-700 transition-colors w-full">
                     Get Started
                   </button>
                 </Link>
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -2,8 +2,8 @@ const Dashboard = () => {
   return (
     <>
       <div className="min-h-full min-w-full">
-        <h1 className="text-2xl font-bold mb-5 text-white">Dashboard</h1>
-        <p className="mb-5 text-white">Welcome to the dashboard!</p>
+        <h1 className="text-2xl font-bold mb-5 text-black">Dashboard</h1>
+        <p className="mb-5 text-black">Welcome to the dashboard!</p>
       </div>
     </>
   );
