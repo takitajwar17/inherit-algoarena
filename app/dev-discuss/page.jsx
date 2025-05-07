ca4d303e31ed4993ec34f70c259482a79b5dd1b6
takitajwar17 <tajwaruzzaman@iut-dhaka.edu>
2025-05-07 11:58:30 +0600

    file cleanup

---
+++ app/dev-discuss/page.jsx
@@ -0,0 +1,12 @@
+const DevDiscuss = () => {
+  return (
+    <>
+      <div className="min-h-full min-w-full">
+        <h1 className="text-2xl font-bold mb-5 text-black">DevDiscuss</h1>
+        <p className="mb-5 text-black">Welcome to DevDiscuss</p>
+      </div>
+    </>
+  );
+};
+
+export default DevDiscuss;
