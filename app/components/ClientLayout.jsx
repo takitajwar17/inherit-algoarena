06586a924e64b8a557b1604dfb5ac6b5099f49ba
takitajwar17 <tajwaruzzaman@iut-dhaka.edu>
2025-05-09 09:21:30 +0600

    On board tour done

--- app/components/ClientLayout.jsx
+++ app/components/ClientLayout.jsx
@@ -1,12 +1,23 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { useState } from "react"; 
-import Header from "./Header"; 
+import { useState, useEffect } from "react";
+import Header from "./Header";
 import Sidebar from "./Sidebar";
+import dynamic from 'next/dynamic';
+
+// Dynamically import TourGuide with no SSR
+const TourGuide = dynamic(() => import("@/components/TourGuide"), {
+  ssr: false,
+});
 
 export default function ClientLayout({ children }) {
   const pathname = usePathname();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
   // Normalize the pathname to handle trailing slashes
   const normalizedPath = pathname.replace(/\/+$/, "");
@@ -15,13 +26,13 @@ export default function ClientLayout({ children }) {
   // Define paths where Sidebar should not be rendered
   const excludedPaths = ["/sign-in", "/sign-up"];
   const isExcludedPath =
-    excludedPaths.includes(normalizedPath) || 
-    pathname === "/not-found" || 
+    excludedPaths.includes(normalizedPath) ||
+    pathname === "/not-found" ||
     pathname.startsWith("/admin");
 
   const shouldRenderSidebar = !isHomePage && !isExcludedPath;
 
-  const [isOpen, setIsOpen] = useState(false); 
+  const [isOpen, setIsOpen] = useState(false);
 
   // Don't render any layout components for admin paths
   if (pathname.startsWith("/admin")) {
@@ -40,13 +51,18 @@ export default function ClientLayout({ children }) {
         className={`w-full transition-all duration-300 ${
           !isHomePage ? "pt-20" : ""
         } ${
-          shouldRenderSidebar && isOpen && window.innerWidth >= 768
+          shouldRenderSidebar && isOpen && isMounted && window.innerWidth >= 768
             ? "pl-56"
-            : shouldRenderSidebar ? "pl-8" : ""
+            : shouldRenderSidebar
+            ? "pl-8"
+            : ""
         }`}
       >
         <div className="flex items-start justify-center min-h-screen w-full">
-          <div className="w-full">{children}</div>
+          <div className="w-full">
+            {children}
+            {isMounted && !isExcludedPath && <TourGuide />}
+          </div>
         </div>
       </main>
     </>
