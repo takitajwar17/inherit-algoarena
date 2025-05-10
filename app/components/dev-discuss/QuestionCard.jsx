c6e003f4ce00ae6521ed19e5f688627c8d0b86ce
takitajwar17 <tajwaruzzaman@iut-dhaka.edu>
2025-05-10 12:56:30 +0600

    userid fetching done

--- app/components/dev-discuss/QuestionCard.jsx
+++ app/components/dev-discuss/QuestionCard.jsx
@@ -1,13 +1,17 @@
 // app/components/dev-discuss/QuestionCard.jsx
 
 "use client";
-
+import { useAuth } from "@clerk/nextjs"; // Adjust based on your auth provider
 
 
 export default function QuestionCard({ question }) {
-  
+  const { userId } = useAuth();
+  const [votes, setVotes] = useState(question.votes);
+  const [userVote, setUserVote] = useState(0); // 1 for upvoted, -1 for downvoted, 0 for none
+ 
+
 
   return (
-     <div><h1>Question card</h1></div>
+     <div><h1>{userId}</h1></div>
   );
 }
