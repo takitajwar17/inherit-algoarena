d55eb333ed714732c6173e96655bc0588cc90d88
takitajwar17 <tajwaruzzaman@iut-dhaka.edu>
2025-05-10 21:33:30 +0600

    raw page formed

---
+++ app/dev-discuss/[Id]/page.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+
+
+export default function QuestionDetailPage({ params }) {
+  
+  return (
+    <main className="min-h-screen bg-background">
+      <div className="max-w-4xl mx-auto px-4 py-6">
+        
+         <h1>Dev dicuss question page id</h1>
+        
+         
+
+        
+
+        
+       
+       
+        
+      </div>
+    </main>
+  );
+}
