8585611ba050924931fe7ce48f678c12343e6308
takitajwar17 <tajwaruzzaman@iut-dhaka.edu>
2025-05-10 18:00:30 +0600

    tested working well

--- app/dev-discuss/ask-question/page.jsx
+++ app/dev-discuss/ask-question/page.jsx
@@ -1,1 +1,13 @@
-// 
+// app/dev-discuss/ask-question/page.jsx
+
+// "use client";
+
+
+export default function AskQuestionPage() {
+
+  return (
+    <main className="min-h-screen bg-background">
+      
+    </main>
+  );
+}
